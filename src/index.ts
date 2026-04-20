bf0e1887080001aae43985047dd1b78778a090ac
Daniel da Silva <danielfdsilva@gmail.com>
2026-04-20 09:53:45 +0100

    Update exports and remove old color frames API (clean break)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export { HealpixCellsLayer } from './layers/healpix-cells-layer';
 export { setWorkerUrl, setWorkerFactory } from './config';
-export { makeColorFrameFromValues } from './utils/color-frame';
 export type { CellIdArray } from './types/cell-ids';
 export type {
   HealpixCellsLayerProps,
+  HealpixFrameObject,
   HealpixScheme
 } from './types/layer-props';
